d25f06e5b87d9b9add722e30af6a6713fc2ce7e4
gm <graham.murphy@gmail.com>
2020-05-27 21:46:40 -0400

    first commit

--- src/components/hero.js
+++ src/components/hero.js
@@ -26,12 +26,11 @@ const Header = ({ siteTitle }) => (
         opacity: 0.5,
       }}
     />
-    <h1 style={{ textAlign: "center" }}>Landing Page Starter</h1>
+    <h1 style={{ textAlign: "center" }}>CustomerOps</h1>
     <p style={{ textAlign: "center", maxWidth: 440 }}>
-      This landing page looks great on all devices and is minimal in design. Add
-      what you want and deploy.
+      Because it would be an absolute shame to have a company without a website.
     </p>
-    <Button>Get Early Access</Button>
+    <Button>Contact Us</Button>
     <div style={{ margin: 60, width: `250px`, position: "relative" }}>
       <div style={{ clipPath: "inset(2% 5% round 2% 5%)" }}>
         <MockupContent />
